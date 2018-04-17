1d3634a0de68e7f66d0e2205f1ceb44573cd0fee
MouliViz <36884726+MouliViz@users.noreply.github.com>
2018-04-17 22:29:39 +0200

    Add files via upload

--- indexbis.js
+++ indexbis.js
@@ -1,29 +1,9 @@
 // PREMIERE ETAPE : Definition des variables globales utilisées par d3 pour le rendu
 // et par d'autres functions dans le code
 
-// A ENLEVER A TERME
-var paris = [2.35, 48.85];
-var lyon = [4.85, 45.75];
-// var marseille = [Longitudebis[6], Latitudebis[6]];
-var toulouse = [1.44, 43.60];
-var cc = [paris, lyon, toulouse];
-
-var formatNumber = d3.format("s"),
-    offset = [width / 2, height / 2],
-    scale  = 150,
-    center = [0, 49.5],
-    width = 1200,
-    height = 700
-    ;
-
-/*var margin = {top: 20, right: 20, bottom: 20, left: 20},
-    padding = {top: 60, right: 60, bottom: 60, left: 60},
-    outerWidth = 960,
-    outerHeight = 600,
-    innerWidth = outerWidth - margin.left - margin.right,
-    innerHeight = outerHeight - margin.top - margin.bottom,
-    width = innerWidth - padding.left - padding.right,
-    height = innerHeight - padding.top - padding.bottom;*/
+var width = 2000,
+    height = 800,
+    formatNumber = d3.format("s");
 
 var projection = d3.geoAlbers()
     .center([0, 49.5])
@@ -35,23 +15,10 @@ var projection = d3.geoAlbers()
 var path = d3.geoPath()
     .projection(projection);
 
-// using the path determine the bounds of the current map and use
-      // these to determine better values for the scale and translation
-//var bounds  = path.bounds(json);
-//var hscale  = scale*width  / (bounds[1][0] - bounds[0][0]);
-//var vscale  = scale*height / (bounds[1][1] - bounds[0][1]);
-//var scale   = (hscale < vscale) ? hscale : vscale;
-//var offset  = [width - (bounds[0][0] + bounds[1][0])/2,height - (bounds[0][1] + bounds[1][1])/2];
-
-//projection = d3.geo.Albers().center(center).rotate([-2.8, 3]).parallels([45, 55]).scale(scale).translate(offset);
-//path = path.projection(projection);
-
 var svg = d3.select('body')
-    .attr("align","center")
     .append("svg")
     .attr("width", width)
-    .attr("height", height)
-    ;
+    .attr("height", height);
 
 // DEUXIEME ETAPE : On va écrire notre fonction pour lire notre tableau CSV
 // Si tu comprends pas le code, passe à la suite, tu as juste besoin de savoir
@@ -71,26 +38,27 @@ function selectCities(data, year) {
     let result_20171980 = [];
     data.forEach((city) => {
         if (Number(city.Annee) == year) {
-        result_20171980.push([city.Ville, Number(city.Longitude), Number(city.Latitude), city.Annee, city.x])
-    }});
-    // let result_total = [];
-    // data.forEach((city) => {
-    //     result_total.push([city.Ville, Number(city.Longitude), Number(city.Latitude), city.Annee])
-    // });
+            result_20171980.push([city.Ville,
+                                  Number(city.longitudebis),
+                                  Number(city.latitudebis),
+                                  city.Annee,
+                                  Number(city.Longitude),
+                                  Number(city.Latitude)])
+        }
+    });
 
     return result_20171980;
-    // return result_total;
-}
+    }
 
 // TROISIEME ETAPE : On dessine !
 function drawFrance(france) {
     var regions = svg.selectAll(".departements")
         .data(topojson.feature(france, france.objects.regions).features)
         .enter()
-        .append("path")
-        .attr("class", "departements")
-        .attr("d", path)
-        .style("fill","lightblue");
+            .append("path")
+            .attr("class", "departements")
+            .attr("d", path)
+            .style("fill","lightblue");
 }
 
 function drawCities(cities) {
@@ -98,49 +66,50 @@ function drawCities(cities) {
 
     circles
         .exit()
-        .remove();
+            .remove();
 
     circles
         .enter()
-        .append("circle")
+            .append("circle")
         .merge(circles)
-        .attr("cx", function (d) {
-            return projection([d[1], d[2]])[0];
-        })
-        .attr("cy", function (d) {
-            return projection([d[1], d[2]])[1];
-        })
-        .attr("r", "2px")
-        .attr("fill", "red");
-
+    		.attr("cx", function (d) {
+                return projection([d[1], d[2]])[0];
+            })
+    		.attr("cy", function (d) {
+                return projection([d[1], d[2]])[1];
+            })
+    		.attr("r", "3px")
+    		.attr("fill", "red");
+    
     return cities;
-}
+ }
+
 
 function drawLines(cities) {
-    var lines = svg.selectAll("line").data(cities)
-
+     var lines = svg.selectAll("line").data(cities)
+    
     lines
         .exit()
-        .remove();
-
+            .remove();
+    
     lines
         .enter()
-        .append("line")
+            .append("line")
         .merge(lines)
-        .attr("x1", function (d) {
-            return projection([d[1], d[2]])[0];
-        })
-        .attr("y1", function (d) {
-            return projection([d[1], d[2]])[1];
-        })
-        .attr("x2", function (d) {
-            return projection([2.3488, 48.85341])[0];
-        })
-        .attr("y2", function (d) {
-            return projection([2.3488, 48.85341])[1];
-        })
-        .attr("stroke-width", 2)
-        .attr("stroke", "#f6931f")
+            .attr("x1", function (d) {
+                return projection([d[1], d[2]])[0];
+            })
+            .attr("y1", function (d) {
+                return projection([d[1], d[2]])[1];
+            })
+            .attr("x2", function (d) {
+                return projection([d[4], d[5]])[0];
+            })
+            .attr("y2", function (d) {
+                return projection([d[4], d[5]])[1];
+            })
+            .attr("stroke-width", 0.5)
+            .attr("stroke", "green")
     ;
     return cities;
 
@@ -150,23 +119,23 @@ function writeNames(cities) {
     var names = svg.selectAll("text").data(cities)
     names
         .enter()
-        .append("svg:text")
+            .append("svg:text")
         .merge(names)
-        .text(function(d) {
-            return (d[0]+'\n'+d[4]);
-        })
-        .attr("x", function(d) {
-            return projection([d[1], d[2]])[0];
-        })
-        .attr("y", function(d) {
-            return  projection([d[1], d[2]])[1];
-        })
-        .attr("text-anchor", "end")
-        .attr('font-size', '10pt');
+            .text(function(d) {
+                return (d[0]);
+            })
+            .attr("x", function(d) {
+                return projection([d[1], d[2]])[0];
+            })
+            .attr("y", function(d) {
+                return  projection([d[1], d[2]])[1];
+            })
+            .attr("text-anchor", "middle")
+            .attr('font-size', '8pt');
 
     names
         .exit()
-        .remove();
+            .remove();
 
     return cities.length;
 };
@@ -177,12 +146,12 @@ function main(err, france) {
 
     Promise.resolve()
         .then(() => drawFrance(france)) // On dessine le fond de carte
-.then(() => readCSV("data_latlong3.csv")) // on prend les données des villes
-.then(data => selectCities(data, selectedYear)) // On les travaille un peu
-.then(cities => drawCities(cities)) // On les dessine
-.then(cities => drawLines(cities))
-.then(cities => writeNames(cities))
-.then(nb => console.log(nb + ' points affichés'));
+        .then(() => readCSV("data_latlong.csv")) // on prend les données des villes
+        .then(data => selectCities(data, selectedYear)) // On les travaille un peu
+        .then(cities => drawCities(cities)) // On les dessine
+        .then(cities => drawLines(cities))
+        .then(cities => writeNames(cities))
+        .then(nb => console.log(nb + ' points affichés'));
 }
 
 // Triggered when input changes
@@ -190,16 +159,16 @@ function handleYearChange() {
     var selectedYear = document.getElementById("amount").value;
 
     Promise.resolve()
-        .then(() => readCSV("data_latlong3.csv")) // on prend les données des villes
-.then(data => selectCities(data, selectedYear)) // On les travaille un peu
-.then(cities => drawCities(cities)) // On les dessine
-.then(cities => drawLines(cities))
-.then(cities => writeNames(cities))
-.then(nb => console.log(nb + ' points affichés'));
+        .then(() => readCSV("data_latlong.csv")) // on prend les données des villes
+        .then(data => selectCities(data, selectedYear)) // On les travaille un peu
+        .then(cities => drawCities(cities)) // On les dessine
+        .then(cities => drawLines(cities))
+        .then(cities => writeNames(cities))
+        .then(nb => console.log(nb + ' points affichés'));
 }
 
 // DERNIERE ETAPE
 //Dès que le JSON de la carte de France est chargé, la fonction main est lancée !
-queue()
+ queue()
     .defer(d3.json, "https://api.myjson.com/bins/dw4e3")
     .await(main);
