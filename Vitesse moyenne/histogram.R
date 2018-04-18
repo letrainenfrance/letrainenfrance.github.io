e9be0e71e000b0f8dc42c2321b57d7f6730791dd
asimcic <36768400+asimcic@users.noreply.github.com>
2018-04-18 19:11:23 +0200

    Add files via upload

--- Vitesse moyenne/histogram.R
+++ Vitesse moyenne/histogram.R
@@ -5,21 +5,25 @@ library(reshape)
 data <- as.data.frame(read_excel("C:/Users/Alexandre/Downloads/selected_cities.xlsx"))
 names(data)[1] <- "annee"
 
+data[60,4] <- data[59,4]
+data[60,2] <- NA
+
 mydata <- melt(data, id=c("annee"))
 names(mydata) <- c("annee","trajet","vitesse")
 
 mydata$paris <- substr(mydata$trajet, 1, 5) == "PARIS"
 mydata$vitesse <- as.numeric(as.character(mydata$vitesse))
 
+
 for(i in as.numeric(data$annee)){
 d1 <- subset(mydata,mydata$annee==i & !is.na(mydata$vitesse))
 d1 <- d1[order(-d1$vitesse),]
 d1$position <- 1:length(d1$annee)
 if(sum(d1$paris)==length(d1$paris)){
-  cbPalette <- c("#009E73")
+  cbPalette <- c("#56B4E9")
 } else {
   cbPalette <- c("#009E73","#56B4E9")
-  }
+}
 
 ggplot(data = d1 , aes(x=position, y=vitesse, fill=paris)) +
   geom_bar(stat = "identity", position=position_dodge()) + 
@@ -30,6 +34,7 @@ ggplot(data = d1 , aes(x=position, y=vitesse, fill=paris)) +
   scale_x_discrete(limits = d1$trajet) +
   labs(title = paste ("Vitesse moyenne en", i , sep = " ", collapse = NULL) , subtitle = "En km/h")+
   xlab(" ") +  ylab(" ") +
+  ylim(0,250)+
   scale_fill_manual(values=cbPalette)
 
 ggsave(filename = paste ("Vitesse moyenne en", i , ".png", sep = " ", collapse = NULL), device = "png" , path = "C:/Users/Alexandre/Desktop/Vitesse Moyenne")
