2450dde1fd584c7de642bb9d3f4051a710f422c3
MouliViz <36884726+MouliViz@users.noreply.github.com>
2018-04-21 14:28:56 +0200

    Update indexqua.js

--- indexqua.js
+++ indexqua.js
@@ -113,7 +113,7 @@ function drawLines2(cities2) {
             })
             .attr("stroke-width", function(d){
     if(d[6] == "2h") {
-        return "2";
+        return "3";
     } else {
         return "2";
     }
